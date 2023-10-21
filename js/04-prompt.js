d0b8050d922858e0c283c594eeb5ea570e909085
CosmeNicolas <nicomas7590@gmail.com>
2023-10-21 19:40:25 -0300

    realice el ejercicio numero 4 usando el metodo prompt y document.write

---
+++ js/04-prompt.js
@@ -0,0 +1,4 @@
+//Escribe un programa de dos lineas que pida el nombre del usuario con un prompt y escriba un texto que diga "Hola nombreUsuario"
+
+let nombreDeUsuario = prompt("Ingrese su nombre de Usuario");
+document.write("Hola usuario " + nombreDeUsuario);
