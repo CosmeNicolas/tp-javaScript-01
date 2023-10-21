9b1b89fe7843ec1b3ee4c066ac7ee8c5b46fba8f
CosmeNicolas <nicomas7590@gmail.com>
2023-10-21 20:36:15 -0300

    realice el ejercicio 5 usando el metodo prompt, parseInt, prompt y variables

---
+++ js/05-ejercioCinco.js
@@ -0,0 +1,4 @@
+//Escribe un programa de tres lineas que pida un número, pida otro número y esrciba el resultado de sumar estos dos numeros
+let numero1 = prompt("ingrese el primer número");
+let numero2 = prompt("ingrese el segundo número");
+document.write("el resultados es...  ") + document.write(parseInt(numero1)+ (parseInt(numero2)));
