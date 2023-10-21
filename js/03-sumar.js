55e911b2af7a23f12de1c0224acf62259f0a6eb1
CosmeNicolas <nicomas7590@gmail.com>
2023-10-21 19:19:26 -0300

    realice el ejercicio numero 3 usando variable let y document.write para imprimir en pantalla el resultado

---
+++ js/03-sumar.js
@@ -0,0 +1,3 @@
+//03- Escribe un programa de una sola linea que escriba en la pantalla el resultado de sumar 3 + 5
+let sumar = 3 + 5; document.write("El resultado de 3 + 5 es... ") + document.write(sumar);
+
