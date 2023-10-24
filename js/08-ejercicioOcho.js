f688e64f57a42c0b6bc5808ddbc3124e00616dd3
CosmeNicolas <nicomas7590@gmail.com>
2023-10-24 15:24:58 -0300

    realice el ejercicio numero ocho utlizando mod 2 para saber si los numeros ingresados son divisibles por dos y si no lo lo sabremos con un alert que nos brinda el mensaje

--- js/08-ejercicioOcho.js
+++ js/08-ejercicioOcho.js
@@ -1,2 +1,12 @@
 //Escribe un programa que pida un número y diga si es divisible por 2
 
+let numeroIngresado = parseInt(prompt("Ingrese su numero"));
+let divisibleDos = numeroIngresado % 2 ;
+
+
+if (divisibleDos == 0) {
+  alert("es divisible en dos")
+} else {
+  alert("no es divisible en dos")
+}
+
