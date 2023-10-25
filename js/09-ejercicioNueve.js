757129c837891802ece8ad2867fc498132fbd841
CosmeNicolas <nicomas7590@gmail.com>
2023-10-25 10:51:44 -0300

    realice el ejercicio 9 usando for

--- js/09-ejercicioNueve.js
+++ js/09-ejercicioNueve.js
@@ -1,2 +1,16 @@
 /*Escribe un programa que pida una frase y escriba las vocales que aparecen
 NOTA: tener en cuenta la función lenght y substring o charAt(developer mozilla)*/
+
+let frase = prompt('ingrese una frase')
+
+
+
+for(let vocales = 0; vocales < frase.length; vocales++){
+  if(frase.charAt(vocales) === 'a'|| frase.charAt(vocales) === 'e'|| frase.charAt(vocales) === 'i'|| frase.charAt(vocales) === 'o'|| frase.charAt(vocales) === 'u'){
+    console.log(frase[vocales])
+    document.writeln(frase[vocales])
+  }
+} 
+
+
+
